e7bdde25f5391acba865fb88863cc6a3c4d575ec
DEffet <161118053+DEffet@users.noreply.github.com>
2024-11-22 23:07:22 +0300

    Create 2.2.py

---
+++ 2.2.py
@@ -0,0 +1,12 @@
+# Ввод данных
+first = int(input("Введите первое число: "))
+second = int(input("Введите второе число: "))
+third = int(input("Введите третье число: "))
+
+# Логика программы
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
